4578c8e44f07371a088d58e3d86b17d486de2adb
Blackoutburst <blackoutburst@hotmail.com>
2022-02-18 23:02:15 +0100

    Added packet scoreboard score

---
+++ src/com.blackoutburst.nms/NMSScoreboardScorePacket.java
@@ -0,0 +1,24 @@
+package com.blackoutburst.nms;
+
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+
+public class NMSScoreboardScorePacket {
+
+    public static void send(Player player, NMSScoreboardScore score) {
+        try {
+            final Class<?> packetClass = NMS.getClass("PacketPlayOutScoreboardScore");
+            final Class<?> scoreClass = NMS.getClass("ScoreboardScore");
+
+            final Constructor<?> packetConstructor = packetClass.getConstructor(scoreClass);
+
+            final Object packet = packetConstructor.newInstance(score);
+
+            NMS.sendPacket(player, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
