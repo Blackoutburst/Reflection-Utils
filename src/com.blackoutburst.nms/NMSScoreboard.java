c893fa7b96addfdd0b14acb8056dcf2216a4b9fe
Blackoutburst <blackoutburst@hotmail.com>
2022-02-18 22:35:54 +0100

    Added display slot method

--- src/com.blackoutburst.nms/NMSScoreboard.java
+++ src/com.blackoutburst.nms/NMSScoreboard.java
@@ -1,22 +1,30 @@
 package com.blackoutburst.nms;
 
+import org.bukkit.scoreboard.DisplaySlot;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
 public class NMSScoreboard {
 
+    public enum DisplaySlot {
+        BELOW_NAME,
+        PLAYER_LIST,
+        SIDEBAR;
+    }
+
     public Object scoreboard;
     public Object objective;
 
-    private Class<?> packetClass;
+    private Class<?> scoreboardClass;
 
     public NMSScoreboard() {
         try {
-            packetClass = NMS.getClass("Scoreboard");
+            scoreboardClass = NMS.getClass("Scoreboard");
 
-            final Constructor<?> packetConstructor = packetClass.getConstructor();
+            final Constructor<?> scoreboardConstructor = scoreboardClass.getConstructor();
 
-            scoreboard = packetConstructor.newInstance();
+            scoreboard = scoreboardConstructor.newInstance();
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -30,10 +38,22 @@ public class NMSScoreboard {
 
             final Object criteria = criteriaConstructor.newInstance(objective);
 
-            final Method method = packetClass.getMethod("registerObjective", String.class, criteriaClass);
+            final Method method = scoreboardClass.getMethod("registerObjective", String.class, criteriaClass);
 
             this.objective = method.invoke(null, name, criteria);
 
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDisplaySlot(DisplaySlot slot) {
+        try {
+            final Class<?> objectiveClass = NMS.getClass("ScoreboardObjective");
+
+            final Method method = scoreboardClass.getMethod("setDisplaySlot", int.class, objectiveClass);
+
+            method.invoke(null, slot.ordinal(), this.objective);
         } catch(Exception e) {
             e.printStackTrace();
         }
