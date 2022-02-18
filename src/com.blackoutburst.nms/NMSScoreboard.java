6eaa47f4d3d1dbf8074fa65aeca64994a6a2e2d2
Blackoutburst <blackoutburst@hotmail.com>
2022-02-18 22:29:59 +0100

    Added ability to register objective

--- src/com.blackoutburst.nms/NMSScoreboard.java
+++ src/com.blackoutburst.nms/NMSScoreboard.java
@@ -1,18 +1,39 @@
 package com.blackoutburst.nms;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
 
 public class NMSScoreboard {
 
     public Object scoreboard;
+    public Object objective;
+
+    private Class<?> packetClass;
 
     public NMSScoreboard() {
         try {
-            final Class<?> packetClass = NMS.getClass("Scoreboard");
+            packetClass = NMS.getClass("Scoreboard");
 
             final Constructor<?> packetConstructor = packetClass.getConstructor();
 
             scoreboard = packetConstructor.newInstance();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void registerObjective(String name, String objective) {
+        try {
+            final Class<?> criteriaClass = NMS.getClass("ScoreboardBaseCriteria");
+
+            final Constructor<?> criteriaConstructor = criteriaClass.getConstructor(String.class);
+
+            final Object criteria = criteriaConstructor.newInstance(objective);
+
+            final Method method = packetClass.getMethod("registerObjective", String.class, criteriaClass);
+
+            this.objective = method.invoke(null, name, criteria);
+
         } catch(Exception e) {
             e.printStackTrace();
         }
