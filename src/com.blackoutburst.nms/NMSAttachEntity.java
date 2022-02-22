96dc623d10bef35d522956208274f93c359d49f7
Blackoutburst <blackoutburst@hotmail.com>
2022-02-22 16:24:20 +0100

    Added Attach Entity packet

---
+++ src/com.blackoutburst.nms/NMSAttachEntity.java
@@ -0,0 +1,24 @@
+package com.blackoutburst.nms;
+
+import org.bukkit.entity.NPC;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Constructor;
+
+public class NMSAttachEntity {
+
+    public static void send(Player player, NMSEntities rider, NMSEntities mount) {
+        try {
+            Class<?> packetClass = NMS.getClass("PacketPlayOutAttachEntity");
+            Class<?> entityClass = NMS.getClass("Entity");
+
+            Constructor<?> packetConstructor = packetClass.getConstructor(int.class, entityClass, entityClass);
+
+            Object packet = packetConstructor.newInstance(0, rider.entity, mount.entity);
+
+            NMS.sendPacket(player, packet);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
