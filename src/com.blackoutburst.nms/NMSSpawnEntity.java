c367a0561ec35ba091741606bdde81be6eca0899
Blackoutburst <blackoutburst@hotmail.com>
2022-03-05 22:27:20 +0100

    Added entity data

--- src/com.blackoutburst.nms/NMSSpawnEntity.java
+++ src/com.blackoutburst.nms/NMSSpawnEntity.java
@@ -12,14 +12,14 @@ public class NMSSpawnEntity {
      * @param player the player that will receive the packet
      * @param entity the entity you wish to spawn
      */
-    public static void send(Player player, NMSEntities entity) {
+    public static void send(Player player, NMSEntities entity, int data) {
         try {
             final Class<?> packetClass = NMS.getClass("PacketPlayOutSpawnEntity");
             final Class<?> entityClass = NMS.getClass("Entity");
 
-            final Constructor<?> packetConstructor = packetClass.getConstructor(entityClass, int.class);
+            final Constructor<?> packetConstructor = packetClass.getConstructor(entityClass, int.class, int.class);
 
-            final Object packet = packetConstructor.newInstance(entity.entity, entity.getNetworkID());
+            final Object packet = packetConstructor.newInstance(entity.entity, entity.getNetworkID(), data);
 
             NMS.sendPacket(player, packet);
         } catch(Exception e) {
